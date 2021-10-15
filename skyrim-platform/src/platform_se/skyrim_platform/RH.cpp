32bbbd7a59e2f1229dbf98ec13efc06be3141ccc
russo-2025 <russo-2025@mail.ru>
2021-10-15 22:34:08 +0300

    cleanup

--- skyrim-platform/src/platform_se/skyrim_platform/RH.cpp
+++ skyrim-platform/src/platform_se/skyrim_platform/RH.cpp
@@ -44,35 +44,6 @@ namespace RE {
 		UInt8  unk0D;		// 0D
 		UInt16 pad0E;		// 0E
 	};
-
-	//---
-	//class ButtonEvent;
-	//class InputEvent;
-	class MouseMoveEvent;
-	class PlayerControlsData;
-	class ThumbstickEvent;
-
-	class PlayerInputHandler
-	{
-	public:
-		inline static constexpr auto RTTI = RTTI_PlayerInputHandler;
-
-		virtual ~PlayerInputHandler() = default;  // 00
-
-		virtual bool CanProcess(InputEvent* a_event) = 0;                                      // 01
-		virtual void ProcessThumbstick(ThumbstickEvent* a_event, PlayerControlsData* a_data) = 0;  // 02 - { return; }
-		virtual void ProcessMouseMove(MouseMoveEvent* a_event, PlayerControlsData* a_data) = 0;    // 03 - { return; }
-		virtual void ProcessButton(ButtonEvent* a_event, PlayerControlsData* a_data) = 0;          // 04 - { return; }
-
-		[[nodiscard]] bool IsInputEventHandlingEnabled() const;
-		void               SetInputEventHandlingEnabled(bool a_enabled);
-
-		// members
-		bool          inputEventHandlingEnabled;  // 08
-		std::uint8_t  pad09;                      // 09
-		std::uint16_t pad0A;                      // 0A
-		std::uint32_t pad0C;                      // 0C
-	};
 }
 
 class EmptyEventHandler : public RE::MenuEventHandler {
