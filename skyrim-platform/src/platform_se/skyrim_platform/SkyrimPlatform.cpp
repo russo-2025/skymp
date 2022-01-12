fa2b97fdd4eaf5a5d8f80d44d194326f161ed97f
alekcey0211 <alekcey_ezh@mai.ru>
2022-01-12 20:17:12 +0300

    need help with bool JsValue

--- skyrim-platform/src/platform_se/skyrim_platform/SkyrimPlatform.cpp
+++ skyrim-platform/src/platform_se/skyrim_platform/SkyrimPlatform.cpp
@@ -144,7 +144,7 @@ public:
 
         std::string ip = settings.GetProperty("server-ip").ToString();
         int uiport = (int)settings.GetProperty("server-port") + 1;
-        bool offlineMode = (bool)settings.GetProperty("offlineMode");
+        bool offlineMode = false; // bool(settings.GetProperty("offlineMode")); NEED HELP
         if (offlineMode) {
           for (auto& fileDir : fileDirs) {
             LoadFiles(GetPathsToLoad(fileDir));
